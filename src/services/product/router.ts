f7052b17915de4a68c5d21c73e43683ad386fb8b
StanleySathler <stanleysathlerpinto@gmail.com>
2023-12-17 21:51:32 -0300

    Setup typebox

--- src/services/product/router.ts
+++ src/services/product/router.ts
@@ -1,32 +1,20 @@
 import { FastifyInstance } from "fastify";
+import { Type } from "@sinclair/typebox";
 
 import * as productController from "./controller";
+import { productResourceSchema } from "./schema";
 
 export const productRouter = async (instance: FastifyInstance) => {
-  instance.addSchema({
-    $id: "productResourceSchema",
-    type: "object",
-    properties: {
-      id: { type: "number" },
-      name: { type: "string" },
-      price: { type: "number" },
-    },
-  });
+  instance.addSchema(productResourceSchema);
 
   instance.get(
     "/",
     {
       schema: {
         response: {
-          200: {
-            type: "object",
-            properties: {
-              data: {
-                type: "array",
-                items: { $ref: "productResourceSchema" },
-              },
-            },
-          },
+          200: Type.Object({
+            data: Type.Array(Type.Ref(productResourceSchema)),
+          }),
         },
       },
     },

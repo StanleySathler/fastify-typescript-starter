9a038b8f64d378d2e97f9a2ce65f58819c85c492
StanleySathler <stanleysathlerpinto@gmail.com>
2024-02-28 07:37:58 -0300

    Setup docker-compose

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { setupApp } from "./utils";
 
+const PORT = 3001;
+const HOST = "::"; // This is required for Docker environments
+
 /*
  * Start server.
  */
 const app = setupApp();
-app.listen({ port: 3001 }, function (err) {
+
+app.listen({ port: PORT, host: HOST }, function (err) {
   if (err) {
     app.log.error(err);
     process.exit(1);
